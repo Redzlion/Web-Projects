a60ca0d00f89650d6b29aba7435e225e2614e5d0
Yusif Alakbarzade <73228864+Redzlion@users.noreply.github.com>
2023-06-12 13:21:16 +0200

    Add files via upload

---
+++ Splash page/main.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector(".sidebar-container");
+
+if (nav) {
+  const toggle = nav.querySelector(".sidebar-toggle");
+  
+  if (toggle) {
+    toggle.addEventListener("click", () => {
+      if (nav.classList.contains("is-active")) {
+        nav.classList.remove("is-active");
+      }
+      else {
+        nav.classList.add("is-active");
+      }
+    });
+    
+    nav.addEventListener("blur", () => {
+      nav.classList.remove("is-active");
+    });
+  }
+}
